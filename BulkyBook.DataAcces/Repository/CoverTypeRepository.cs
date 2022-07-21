fd512776e206d63b6e3f7be6927625c94a791159
Zsolt <zsolt.kulcsar@itperspectives.com>
2022-07-21 14:46:00 +0300

    first commit

---
+++ BulkyBook.DataAcces/Repository/CoverTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAcces.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBookWeb.DataAcces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAcces.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType obj)
+        {
+            _db.CoverTypes.Update(obj);
+        }
+    }
+}
